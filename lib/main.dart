27ce8d9abad8fa41ec961d0f2b69f8a0e0f05e4b
DELL <mr2079@fayoum.edu.eg>
2025-01-14 00:59:40 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:todo_app/home.dart';
+
+void main (){
+
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+const MyApp({super.key});
+
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+
+  initialRoute:HomeScreen.routeName ,
+  routes:{
+    HomeScreen.routeName:(context)=>HomeScreen()
+
+
+  } ,
+
+);
+}
+
+
+
+
+
+
+}
